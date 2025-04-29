8c53f1ac648ac45474e56af662b43081abf6d374
Luiscuatro <luisotonico@gmail.com>
2025-04-29 23:00:08 +0200

    feat: add Booking entity

---
+++ src/main/java/com/example/application/model/Booking.java
@@ -0,0 +1,16 @@
+package com.example.application.model;
+
+public class Booking {
+    private String bookingId;
+    private String startDate;
+    private String endDate;
+
+    public String getBookingId() { return bookingId; }
+    public void setBookingId(String bookingId) { this.bookingId = bookingId; }
+
+    public String getStartDate() { return startDate; }
+    public void setStartDate(String startDate) { this.startDate = startDate; }
+
+    public String getEndDate() { return endDate; }
+    public void setEndDate(String endDate) { this.endDate = endDate; }
+}
