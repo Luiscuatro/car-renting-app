3030ad39fe9c5f20b5d4c0d201954157896b797f
Luiscuatro <Luisotonico@gmail.com>
2025-05-05 12:04:05 +0200

    feat: add UserEndpoint with save and get methods

---
+++ src/main/java/com/example/application/endpoint/UserEndpoint.java
@@ -0,0 +1,27 @@
+package com.example.application.endpoint;
+
+import com.example.application.model.User;
+import com.example.application.repository.UserRepositoryImpl;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.Endpoint;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Endpoint
+@AnonymousAllowed
+public class UserEndpoint {
+
+    private final UserRepositoryImpl userRepository;
+
+    @Autowired
+    public UserEndpoint(UserRepositoryImpl userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user) {
+        userRepository.saveUser(user);
+    }
+
+    public User getUser(String userId) {
+        return userRepository.getUser(userId);
+    }
+}
