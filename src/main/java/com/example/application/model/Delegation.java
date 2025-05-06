96071ea07cc4aa13336c70568962014fc35e5d7c
Luiscuatro <Luisotonico@gmail.com>
2025-05-06 09:54:28 +0200

    feat: add Delegation entity with DynamoDB annotations

---
+++ src/main/java/com/example/application/model/Delegation.java
@@ -0,0 +1,77 @@
+package com.example.application.model;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.util.List;
+
+@DynamoDbBean
+public class Delegation {
+    private String pk;
+    private String sk;
+
+    private String delegationId;
+    private String name;
+    private String city;
+    private String address;
+    private List<Car> cars;
+
+    @DynamoDbPartitionKey
+    public String getPk() {
+        return pk;
+    }
+
+    public void setPk(String pk) {
+        this.pk = pk;
+    }
+
+    @DynamoDbSortKey
+    public String getSk() {
+        return sk;
+    }
+
+    public void setSk(String sk) {
+        this.sk = sk;
+    }
+
+    public String getDelegationId() {
+        return delegationId;
+    }
+
+    public void setDelegationId(String delegationId) {
+        this.delegationId = delegationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+}
