e60dba17e3cb3ac87bf78c19e59f753f222ed460
Luiscuatro <Luisotonico@gmail.com>
2025-05-15 10:45:42 +0000

    feat: added CalendarAvailability

---
+++ src/main/java/com/example/application/model/CalendarAvailability.java
@@ -0,0 +1,42 @@
+package com.example.application.model;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.util.List;
+
+@DynamoDbBean
+public class CalendarAvailability {
+
+    private String carId;
+g    private String operation;
+
+    private List<String> availableDates;
+
+    @DynamoDbPartitionKey
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    @DynamoDbSortKey
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public List<String> getAvailableDates() {
+        return availableDates;
+    }
+
+    public void setAvailableDates(List<String> availableDates) {
+        this.availableDates = availableDates;
+    }
+}
